bf8fce63790d07ff940587a0bad1f9ca77179b2e
KIlnur <kilnur1988@rambler.ru>
2025-09-18 20:56:54 +0500

    Добавлены функции

---
+++ js/functions.js
@@ -0,0 +1,28 @@
+const STRING_MAX_LENGTH = 20;
+
+function isStringValid(str) {
+  if(!str.length) {
+    return false;
+  }
+
+  return str.length <= STRING_MAX_LENGTH;
+}
+
+function isPalindromSimple(str) {
+  if(!str.length) {
+    return false;
+  }
+
+  const lower = str.toLowerCase();
+  return lower === lower.split('').reverse().join('');
+}
+
+function isPalindromHard(str) {
+  if(!str.length) {
+    return false;
+  }
+
+  const joinedStr = str.toLowerCase().replaceAll(' ', '');
+  return joinedStr === joinedStr.split('').reverse().join('');
+}
+
